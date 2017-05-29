61af7db8280812638c879135b8cfce64d7f70438
Ed <ed.keen23@gmail.com>
2017-05-29 14:03:40 -0500

    First commit to accounts branch

--- InvestApp/models.py
+++ InvestApp/models.py
@@ -7,7 +7,18 @@ class etf(models.Model):
     name = models.CharField(max_length=200)
     currentPrice = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     owner = models.ForeignKey(User)
-
     def __str__(self):
         """Return a string representation of the model."""
         return '%s %s $%s'%(self.symbol, self.name, self.currentPrice)
+
+class Account(models.Model):
+    owner = models.ForeignKey(User)
+    accountName = models.CharField(max_length=200)
+    accountType = models.CharField(max_length=200)
+    totalCashValue = models.DecimalField(max_digits=6, decimal_places=2)
+    totalStockValue = DecimalField(max_digits=6, decimal_places=2)
+    totalAccountValue = DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.accountName
